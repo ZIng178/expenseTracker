29908f977295c4720302bbf6c34ec03cd24d2464
Wangdi Tenzing <wangdi457@gmail.com>
2021-05-13 14:17:26 -0400

    heroku deployment

---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
+
+const TransactionList = () => {
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  return (
+    <div>
+      <h3> History</h3>
+      <ul id="list" className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionList;
